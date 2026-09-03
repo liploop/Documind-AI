a71d1c99273e7601f8af1033fa35151f864c9105
Ananda Allif Agita <71740159+liploop@users.noreply.github.com>
2026-09-03 19:46:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 
 # Load Environment Variables
 load_dotenv(override=True)
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = st.secrets.get("GEMINI_API_KEY") or os.getenv("GEMINI_API_KEY")
 if api_key:
     genai.configure(api_key=api_key)
 
